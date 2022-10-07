f04b6b799856ff7d6215ec7d73b4a018ac9c38ce
fatihsamur <a.fatihsamur@gmail.com>
2022-10-07 08:56:49 +0300

    first commit

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,18 +1,76 @@
+/* eslint-disable */
 import React from 'react';
 import './Carousel.scss';
 
-const Carousel: React.FC = () => (
-  <div className="Carousel">
-    <ul className="Carousel__list">
-      <li><img src="./img/1.png" alt="1" /></li>
-      <li><img src="./img/1.png" alt="2" /></li>
-      <li><img src="./img/1.png" alt="3" /></li>
-      <li><img src="./img/1.png" alt="4" /></li>
-    </ul>
+type Props = {
+  images: string[],
+  step: number,
+  frameSize: number,
+  itemWidth: number,
+  animationDuration: number,
+  infinite: boolean,
+};
 
-    <button type="button">Prev</button>
-    <button type="button">Next</button>
-  </div>
-);
+type State = {
+  images: string[],
+  translation: number,
+};
+
+class Carousel extends React.Component<Props, State> {
+  state = {
+    images: this.props.images,
+    translation: 0,
+  };
+
+  scrollNext = () => {
+    console.log('scrolled next');
+    this.setState(prevState => ({
+      translation: prevState.translation - 130,
+    }))
+  };
+
+  scrollPrev = () => {
+    console.log('scrolled prev');
+    this.setState(prevState => ({
+      translation: prevState.translation + 130,
+    }))
+  };
+
+  render() {
+    const inlineStyle = {
+      transform: `translateX(${this.state.translation}px)`,
+    };
+    return (
+      <div className="Carousel">
+        <ul className="Carousel__list">
+          {this.state.images.map((image, i) => (
+            <li className="Carousel__list-item" key={image}>
+              <img
+                style={inlineStyle}
+                className="Carousel__img"
+                src={image}
+                alt={`${i}`}
+              />
+            </li>
+          ))}
+        </ul>
+
+        <button
+          type="button"
+          onClick={this.scrollPrev}
+        >
+          Prev
+        </button>
+        <button
+          data-cy="Next"
+          type="button"
+          onClick={this.scrollNext}
+        >
+          Next
+        </button>
+      </div>
+    );
+  }
+}
 
 export default Carousel;
